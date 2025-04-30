b1ccf89a4e046882ce71a6e21ff149a4442e439b
steve <gadafisteve001@gmail.com>
2025-04-30 14:21:34 +0300

    made changes

--- exam_portal/urls.py
+++ exam_portal/urls.py
@@ -5,6 +5,9 @@ from . import views
 urlpatterns = [
     path('login/', views.custom_login , name='login'),
     path('register/', views.register, name='register'),
+    path('logout/', views.custom_logout, name='logout'),
+    path('forgot-password/', views.forgot_password, name='forgot_password'),
+    path('reset-password/<uidb64>/<token>/', views.reset_password, name='reset_password'),
     # ... previous URLs ...
     path('student/', views.student_dashboard, name='student_dashboard'),
     path('school-admin/', views.school_admin_dashboard, name='school_admin_dashboard'),
